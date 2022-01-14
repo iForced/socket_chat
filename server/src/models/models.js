77d14d491d7a97d2359605e97ecdb6fac0fd0b65
iForced <iforce@2ch.hk>
2022-01-14 22:22:14 +0300

    merge models in one file

--- server/src/models/models.js
+++ server/src/models/models.js
@@ -1,6 +1,13 @@
 import sequelize from '../db.js'
 import { DataTypes } from 'sequelize'
 
+export const Message = sequelize.define('message', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    senderId: {type: DataTypes.STRING, allowNull: false},
+    receiverId: {type: DataTypes.STRING, allowNull: false},
+    text: {type: DataTypes.STRING, allowNull: false},
+})
+
 export const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     login: {type: DataTypes.STRING, unique: true, allowNull: false},

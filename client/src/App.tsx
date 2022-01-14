c27312ad0ad9e7d6664e12d2bd6ff0aa0e8d4e2e
iForced <iforce@2ch.hk>
2022-01-14 18:59:33 +0300

    make connect client to server with socket, configure cors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import io from 'socket.io-client'
 
 const App = () => {
+
+    useEffect(() => {
+        const socket = io('http://localhost:5000')
+    }, [])
+
   return (
       <div>
 
--- server/src/index.js
+++ server/src/index.js
@@ -8,12 +8,23 @@ dotenv.config()
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credential: true,
+}))
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
 
 io.on('connection', (socket) => {
     console.log('User connected')
